5b3a381216d7d2a3d458519447211ec6a4da420f
anis fateha natasha ardhi <48323125+AnisFatehaNatasha@users.noreply.github.com>
2019-06-09 23:04:44 +0800

    Update ftp_client.c

--- ftp_client.c
+++ ftp_client.c
@@ -2,7 +2,21 @@
  
 int main(void)
 {
+ 
     netbuf *nbuf;
+ int ftpCommand
+    (
+    int    ctrlSock, /* fd of control connection socket */
+    char * fmt,      /* format string of command to send */
+    int    arg1,     /* first of six args to format string */
+    int    arg2,
+    int    arg3,
+    int    arg4,
+    int    arg5,
+    int    arg6
+    )
+  ftpCommand (ctrlSock, "TYPE I", 0, 0, 0, 0, 0, 0);     /* image-type xfer */
+  ftpCommand (ctrlSock, "STOR %s", file, 0, 0, 0, 0, 0); /* init file write */
  
     FtpInit();
     FtpConnect("kernel.org", &nbuf);
