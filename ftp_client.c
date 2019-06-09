31f8eeae0a24eaab6f7bac781ca38c9bba1e9e92
anis fateha natasha ardhi <48323125+AnisFatehaNatasha@users.noreply.github.com>
2019-06-09 21:29:35 +0800

    Update ftp_client.c

--- ftp_client.c
+++ ftp_client.c
@@ -1,13 +1,9 @@
- 
-=={{header|C}}==
-Using [http://nbpfaus.net/~pfau/ftplib/ ftplib]
-<lang c>
 #include <ftplib.h>
-
+ 
 int main(void)
 {
     netbuf *nbuf;
-
+ 
     FtpInit();
     FtpConnect("kernel.org", &nbuf);
     FtpLogin("anonymous", "", nbuf);
@@ -16,8 +12,7 @@ int main(void)
     FtpDir((void*)0, ".", nbuf);
     FtpGet("ftp.README", "README", FTPLIB_ASCII, nbuf);
     FtpQuit(nbuf);
-
+ 
     return 0;
 }
-</lang>
-
+ 
